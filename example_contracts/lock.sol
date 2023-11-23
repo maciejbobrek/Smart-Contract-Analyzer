b3671c6eef69bea97aec51019e03260b256f14b9
maciejborek <maciejbobrek4@gmail.com>
2023-11-23 22:29:09 +0100

    having fun with contracts

--- example_contracts/lock.sol
+++ example_contracts/lock.sol
@@ -1,5 +1,5 @@
-    // SPDX-License-Identifier: UNLICENSED
-    pragma solidity ^0.8.9;
+
+    pragma solidity ^0.6.0;
 
     contract Lock {
         uint public unlockTime;
--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-import numpy as np
-import solidity
+from eth_utils import address
+from solcx import compile_standard, install_solc
+from dotenv import load_dotenv
+import json
 
 
+install_solc("0.6.0")
 
-
-with open("./example_contracts/lock.sol", "r") as file:
+with open("./example_contracts/simple.sol", "r") as file:
     simple_storage_file = file.read()
+
+
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"simple.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
