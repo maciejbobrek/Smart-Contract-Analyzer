c7a58544726f9ff35886d70b7d83d2b9602c7b8f
maciejborek <maciejbobrek4@gmail.com>
2024-02-02 01:49:48 +0100

    added images to readme

---
+++ script_output/visibility/lock6.sol
@@ -0,0 +1,30 @@
+
+    pragma solidity ^0.8.0;
+
+    contract Lock {
+        uint public unlockTime;
+
+        mapping(address => uint256) private balances;
+
+        event Withdrawal(uint amount, uint when);
+
+        constructor(uint _unlockTime) payable {
+            require(
+                block.timestamp < _unlockTime,
+                "Unlock time should be in the future"
+            );
+
+            unlockTime = _unlockTime;
+        }
+
+        function withdraw(uint amount) public {
+            uint bal = balances[msg.sender];
+            require(block.timestamp >= unlockTime, "You can't withdraw yet");
+            require(amount > 0, "Nothing to withdraw");
+            (bool sent,) = address(msg.sender).call{value: amount}("");
+            require(sent);
+            emit Withdrawal(address(this).balance, block.timestamp);
+
+            balances[msg.sender] -= amount;
+        }
+    }
